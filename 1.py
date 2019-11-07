122127cadd69c16ce7f9f6032df0fbb4a88d98d9
RudyHarunArrasyid <rdyhrn@gmail.com>
2019-11-07 10:34:26 +0700

    komen git

--- 1.py
+++ 1.py
@@ -5,3 +5,11 @@ class X:
         self.nama='Andi'
 Andi=X()
 print(Andi.nama)
+
+#git log
+#git log --oneline
+#git add (namafile)
+#git checkout (id)
+#git checkout master
+#git reset (id)
+#git commit -m "komentar apa aja"
