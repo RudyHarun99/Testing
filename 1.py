c29c3e1769d8f60480e64e86d8da6f1503d9a0de
RudyHarunArrasyid <rdyhrn@gmail.com>
2019-11-07 11:28:53 +0700

    upload github

--- 1.py
+++ 1.py
@@ -18,5 +18,5 @@ print(Andi.nama)
 #git branch -D (nama branch) #hapus branch
 #git checkout -b (namabranch) #bikin branch langsung kerja
 #git merge (nama branch) #merge branch ke master
-#git remote add (nama alias) https:....
+#git remote add (nama alias) https:.... #upload ke github
 #git push (nama alias) master
